319b28fea3f949bfe3244729b28749d50bad4a22
travrei <qjogos7@gmail.com>
2024-10-23 15:01:46 -0300

    Trying to add serviceworker

---
+++ public/sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "nurse-cache";
+
+// Add whichever assets you want to pre-cache here:
+const PRECACHE_ASSETS = ["/assets/", "/"];
+
+// Listener for the install event - pre-caches our assets list on service worker install.
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(PRECACHE_ASSETS);
+    })(),
+  );
+});
